227d2aac1868f5152d12f4c25c53dec5139a2733
JinHee <uh6717@naver.com>
2015-12-21 14:47:59 +0900

    2015-12-21 이진희 SignUp4Activity 생성  - 사진 갤러리에서 crop 후 imgButton 에 먹임  - 사진을 원형으로 처리하는 이벤트는 아직 구현 x  - 수정/삭제가 스토리보드에 있는데, 일단 클릭이벤트 통해서 자동으로 덮어쓰기가 됨

--- app/src/main/java/kr/co/kj_studio/exbuddy/activities/SignUp4Activity.java
+++ app/src/main/java/kr/co/kj_studio/exbuddy/activities/SignUp4Activity.java
@@ -1,19 +1,26 @@
 package kr.co.kj_studio.exbuddy.activities;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 import kr.co.kj_studio.exbuddy.R;
 
 public class SignUp4Activity extends BaseActivity {
 
-    private Button signupPhotoBtn1;
-    private Button signupPhotoBtn2;
-    private Button signupPhotoBtn3;
-    private Button signupPhotoBtn4;
+    private ImageButton signupPhotoBtn1;
+    private ImageButton signupPhotoBtn2;
+    private ImageButton signupPhotoBtn3;
+    private ImageButton signupPhotoBtn4;
     private Button startBtn;
+    private ArrayList<ImageButton> galleryBtnArray = new ArrayList<ImageButton>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +36,11 @@ public class SignUp4Activity extends BaseActivity {
     public void bindViews() {
         super.bindViews();
         this.startBtn = (Button) findViewById(R.id.startBtn);
-        this.signupPhotoBtn4 = (Button) findViewById(R.id.signupPhotoBtn4);
-        this.signupPhotoBtn3 = (Button) findViewById(R.id.signupPhotoBtn3);
-        this.signupPhotoBtn2 = (Button) findViewById(R.id.signupPhotoBtn2);
-        this.signupPhotoBtn1 = (Button) findViewById(R.id.signupPhotoBtn1);
+        this.signupPhotoBtn4 = (ImageButton) findViewById(R.id.signupPhotoBtn4);
+        this.signupPhotoBtn3 = (ImageButton) findViewById(R.id.signupPhotoBtn3);
+        this.signupPhotoBtn2 = (ImageButton) findViewById(R.id.signupPhotoBtn2);
+        this.signupPhotoBtn1 = (ImageButton) findViewById(R.id.signupPhotoBtn1);
+        setGalleryButtonsToArray();
     }
 
     @Override
@@ -41,11 +49,70 @@ public class SignUp4Activity extends BaseActivity {
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mIntent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent mIntent = new Intent(getApplicationContext(), MainActivity.class);
                 finishAllActivities();
                 startActivity(mIntent);
 //                finish();
             }
         });
+
+        setGalleryCallEvent();
+    }
+
+    private void setGalleryButtonsToArray() {
+        galleryBtnArray.add(signupPhotoBtn1);
+        galleryBtnArray.add(signupPhotoBtn2);
+        galleryBtnArray.add(signupPhotoBtn3);
+        galleryBtnArray.add(signupPhotoBtn4);
+    }
+
+    private void setGalleryCallEvent() {
+        for (int i = 0; i < galleryBtnArray.size(); i++) {
+            final int mBtnNum = i;
+            galleryBtnArray.get(mBtnNum).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent mIntent = new Intent();
+                    mIntent.setType("image/*");
+                    mIntent.setAction(Intent.ACTION_GET_CONTENT);
+                    mIntent.putExtra("crop", "true");
+                    mIntent.putExtra("aspectX", 1);
+                    mIntent.putExtra("aspectY", 1);
+                    mIntent.putExtra("outputX", 500);
+                    mIntent.putExtra("outputY", 500);
+                    try {
+                        mIntent.putExtra("return-data", true);
+                        startActivityForResult(Intent.createChooser(mIntent,
+                                "Complete action using"), mBtnNum);
+                    } catch (ActivityNotFoundException e) {
+                        // Do nothing for now
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            Bitmap image = extras.getParcelable("data");
+
+            switch (requestCode) {
+                case 0:
+                    galleryBtnArray.get(0).setImageBitmap(image);
+                    break;
+                case 1:
+                    galleryBtnArray.get(1).setImageBitmap(image);
+                    break;
+                case 2:
+                    galleryBtnArray.get(2).setImageBitmap(image);
+                    break;
+                case 3:
+                    galleryBtnArray.get(3).setImageBitmap(image);
+                    break;
+            }
+        }
     }
 }
