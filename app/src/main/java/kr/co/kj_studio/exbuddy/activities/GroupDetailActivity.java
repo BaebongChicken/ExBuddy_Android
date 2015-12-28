3194dd5244108288f843d2aaad79514907665020
cho881020 <cho881020@nate.com>
2015-12-29 01:03:13 +0900

    2015-12-29 작업마무리
     그룹 디테일에 구글지도 추가함
    
     스크롤뷰 안에서도 스크롤 가능하게 수정함
    
     구글지도 짱짱맨 다음지도 내다버리자

--- app/src/main/java/kr/co/kj_studio/exbuddy/activities/GroupDetailActivity.java
+++ app/src/main/java/kr/co/kj_studio/exbuddy/activities/GroupDetailActivity.java
@@ -1,15 +1,29 @@
 package kr.co.kj_studio.exbuddy.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.PopupMenu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import kr.co.kj_studio.exbuddy.Fragments.WorkaroundMapFragment;
 import kr.co.kj_studio.exbuddy.R;
 
 public class GroupDetailActivity extends BaseActivity {
@@ -23,6 +37,10 @@ public class GroupDetailActivity extends BaseActivity {
     private android.widget.ImageView checkAllMemberImageView;
     private android.widget.FrameLayout googleMapLayout;
     private android.widget.LinearLayout replyLayout;
+
+    private MapFragment mapFragment;
+    private GoogleMap mMap;
+    private android.widget.ScrollView mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +57,7 @@ public class GroupDetailActivity extends BaseActivity {
     public void bindViews() {
         super.bindViews();
 
+        this.mScrollView = (ScrollView) findViewById(R.id.mScrollView);
         this.replyLayout = (LinearLayout) findViewById(R.id.replyLayout);
         this.googleMapLayout = (FrameLayout) findViewById(R.id.googleMapLayout);
         this.checkAllMemberImageView = (ImageView) findViewById(R.id.checkAllMemberImageView);
@@ -48,6 +67,51 @@ public class GroupDetailActivity extends BaseActivity {
         this.hostMessageTxt = (TextView) findViewById(R.id.hostMessageTxt);
         this.numOfMemberTxt = (TextView) findViewById(R.id.numOfMemberTxt);
         this.groupNameTxt = (TextView) findViewById(R.id.groupNameTxt);
+
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+                mMap = googleMap;
+
+                LatLng sydney = new LatLng(-33.867, 151.206);
+                if (ActivityCompat.checkSelfPermission(GroupDetailActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(GroupDetailActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+                googleMap.addMarker(new MarkerOptions()
+                        .title("Sydney")
+                        .snippet("The most populous city in Australia.")
+                        .position(sydney));
+
+                googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+                    @Override
+                    public void onCameraChange(CameraPosition cameraPosition) {
+                        Log.d("CHO", "카메라가 움직여유!");
+                    }
+                });
+
+                ((WorkaroundMapFragment) getFragmentManager().findFragmentById(R.id.map)).setListener(new WorkaroundMapFragment.OnTouchListener() {
+                    @Override
+                    public void onTouch() {
+                        mScrollView.requestDisallowInterceptTouchEvent(true);
+                    }
+                });
+            }
+        });
+
+
 
     }
 
